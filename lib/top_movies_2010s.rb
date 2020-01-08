aa24eff570275e5891466fbbded1c5cf242fb8e2
Joseph Ruiz <ruizj3@uw.edu>
2020-01-08 03:41:30 +0000

    first commit

---
+++ lib/top_movies_2010s.rb
@@ -0,0 +1,6 @@
+require "top_movies_2010s/version"
+
+module TopMovies2010s
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/top_movies_2010s/version.rb
@@ -0,0 +1,3 @@
+module TopMovies2010s
+  VERSION = "0.1.0"
+end
