7bcae2345e8bbc0b911eee5021493d21ab3611e1
Joseph Ruiz <ruizj3@uw.edu>
2020-01-08 04:33:48 +0000

    started making basic methods.

--- lib/top_movies_2010s/cli.rb
+++ lib/top_movies_2010s/cli.rb
@@ -2,6 +2,34 @@ class TopMovies2010s::CLI
   
   def call
     puts "Top Movies for the 2010s!"
+    list_movies
+    pick_one
   end 
+  
+  def list_movies
+    puts "names of movies"
+  end
+  
+  def pick_one
+    puts "Enter which movie rank you want to see:"
+    input = nil
+    while input != "done"
+      input = gets.strip.downcase 
+    
+      case input 
+      when "1"
+        puts "good"
+      when "2"
+        puts "bad"
+      when "list"
+        list_movies
+      else 
+        puts "No data for that rank!"
+    end
+  end
+    
+  end
+  
+  
   
 end 
