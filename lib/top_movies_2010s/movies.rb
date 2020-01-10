8c7709b6eab685fb2cc6eb962d7c94e337ab1a0e
Joseph Ruiz <ruizj3@uw.edu>
2020-01-10 00:16:24 +0000

    changed to grab 11th movie details to test for loop

--- lib/top_movies_2010s/movies.rb
+++ lib/top_movies_2010s/movies.rb
@@ -15,13 +15,13 @@ class TopMovies2010s::Movies
   
   def self.scrape_avi
     desc = Nokogiri::HTML(open("https://www.imdb.com/list/ls003501243/"))
-    rank = desc.search("h3")[0].text.dump.gsub('\n', '').gsub(' ','').scan(/\d+/).first.to_i
-    title = desc.search("h3")[0].text.dump.gsub('\n', '').gsub(' ','').split(".")[1].split("(")[0]
-    release_year = desc.search("h3")[0].text.dump.gsub('\n', '').gsub(' ','').split(")")[1].split(")")[0].gsub("(",'').to_i
+    rank = desc.search("h3")[10].text.dump.gsub('\n', '').gsub(' ','').scan(/\d+/).first.to_i
+    title = desc.search("h3")[10].text.dump.gsub('\n', '').gsub(' ','').split(".")[1].split("(")[0]
+    release_year = desc.search("h3")[10].text.dump.gsub('\n', '').gsub(' ','').split(")")[1].split(")")[0].gsub("(",'').to_i
     
-    mpaa_rating = desc.search("span.certificate").text[0]
-    runtime = desc.search("span.runtime").text.gsub("min",",").gsub(' ','').split(',').each { |c| puts c}[0].to_i
-    genre = desc.search("span.genre").text[0]
+    mpaa_rating = desc.search("span.certificate").text[10]
+    runtime = desc.search("span.runtime").text.gsub("min",",").gsub(' ','').split(',').each { |c| puts c}[10].to_i
+    genre = desc.search("span.genre").text[10]
 
     
     binding.pry 
